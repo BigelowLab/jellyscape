466e87a4505e66a15e72be4efaeda1030ed05f20
Ben Tupper <btupper@bigelow.org>
2025-02-21 14:02:46 -0500

    add abundance tools

--- functions/abundance.R
+++ functions/abundance.R
@@ -1,6 +1,7 @@
-tally_abundance_one = function(x, y, fun = sum,
-                           transform = NULL, 
-                           ...){
+tally_by_poly = function(x, y, fun = sum,
+                         na.rm = TRUE,
+                         transform = NULL, 
+                         ...){
 
   #' Compute abundance across a set of polygons (like hexagons)
   #' 
@@ -10,15 +11,24 @@ tally_abundance_one = function(x, y, fun = sum,
   #' @param ... other arguments for fun
   #' @param transform a function for transforming the results
   #' @return sf table of aggregate per polygon
-  r = aggregate(x, sf::st_geometry(y), fun, ...)
-  if (!inherits(r, "tbl_df")){
-    crs = sf::st_crs(r)
-    gcol = attr(r, "sf_column")
-    r = dplyr::as_tibble(r) |>
-      sf::st_as_sf(sf_column_name = gcol, crs  = crs)
-  }
-  if (!is.null(transform)) r[[1]] = transform(r[[1]] + 0.0001)
-  r  
+    
+    value= st_contains(y, x) |>
+      sapply(
+        function(ix, x = NULL){
+          r = if (length(ix) == 0) {
+            NA_real_
+          } else {
+            x |>
+              dplyr::slice(ix) |>
+              dplyr::pull(var = "value") |>
+              fun(na.rm = na.rm)
+          }
+        }, x = x )
+    
+    if(!is.null(transform)) value = transform(value)
+    
+    y |>
+      dplyr::mutate(value = value, .before = 1)
 }
 
 
@@ -27,7 +37,8 @@ tally_abundance = function(x = read_ecomon_spp(form = 'sf') |> ecomon_to_long(),
                            y = read_hexbin(), 
                            fun = sum,
                            transform = NULL, 
-                           na.rm = TRUE){
+                           na.rm = TRUE,
+                           shape = "long"){
   
   #' Compute abundance across a set of polygons (like hexagons)
   #' 
@@ -36,18 +47,73 @@ tally_abundance = function(x = read_ecomon_spp(form = 'sf') |> ecomon_to_long(),
   #' @param fun name of the function to apply
   #' @param ... other arguments for fun
   #' @param transform a function for transforming the results
+  #' @param shape chr one of "long" (default) ior "wide"
   #' @return sf table of aggregate per polygon
   
-  
-  x |>
+  geom_col = attr(y, "sf_column")
+  r = x |>
     dplyr::select(dplyr::all_of(c("name", "value"))) |>
     dplyr::group_by(name) |>
     dplyr::group_map(
-      function(tbl, key, fun = sum, transform = log1p, ...){
-        tally_abundance_one(tbl, y, fun = fun, transform = transform) |>
+      function(tbl, key, fun = NULL, transform = NULL, na.rm = NULL){
+        tally_by_poly(tbl, y, fun = fun, transform = transform, na.rm = na.rm) |>
           dplyr::mutate(name = key$name, .before = 1) 
       }, fun = fun, transform = transform, na.rm = na.rm) |>
     dplyr::bind_rows() |>
-    tidyr::pivot_wider(names_from = name, values_from = value) |>
-    dplyr::relocate(geometry, .after = dplyr::last_col())
+    dplyr::relocate(dplyr::all_of(geom_col), .after = dplyr::last_col())
+  
+    if (tolower(shape[1]) == "wide"){
+      r = wider_abundance(r)
+    }
+  r
 }
+
+wider_abundance = function(x){
+  #' Pivot a long-form abundance table to wide
+  #' 
+  #' @param x the long-form table
+  #' @return pivoted table
+  tidyr::pivot_wider(x, 
+                     names_from = dplyr::all_of("name"), 
+                     values_from = dplyr::all_of("value"))
+}
+
+longer_abundance = function(x, pattern = "^.*_10m2$"){
+  #' Pivot a long-form abundance table to wide
+  #' 
+  #' @param x the long-form table
+  #' @param pattern chr, regex for [dplyr::matches]
+  #' @return pivoted table
+  tidyr::pivot_longer(x, 
+                     values_from = dplyr::matches(pattern), 
+                     values_to = "value",
+                     names_to = "name")
+}
+
+
+write_abundance = function(x, 
+                           name = "abundance",
+                           path =  here::here("data", "abundance")){
+  #' Write a abundance spatial object
+  #' 
+  #' @param x sf hexgrid object
+  #' @param name chr, the name of the abundance object
+  #' @param path chr the directory where the file is written
+  #' @return the input object `x`
+  #' 
+  path = make_path(path)
+  filename = file.path(path, paste0(name, ".gpkg"))
+  sf::write_sf(x, filename)
+}
+
+read_abundance= function(name = "abundance",
+                         path =  here::here("data", "abundance")){
+  #' Read a abundance
+  #' 
+  #' @param name chr, the name of the abundance object
+  #' @param path chr the directory where the file is written
+  #' @return an object read by [sf::read_sf]
+  filename = file.path(path, paste0(name, ".gpkg"))
+  sf::read_sf(filename)
+}
+
--- functions/misc.R
+++ functions/misc.R
@@ -10,3 +10,13 @@ read_coast = function(scale = "medium", form = "sf"){
   rnaturalearth::ne_coastline(scale = scale[1], returnclass = form[1]) |>
     sf::st_geometry()
 }
+
+
+make_path = function(path){
+  #' Safely make a path
+  #' @param path chr the path description
+  #' @return the path description (whether of not it exists)
+  ok = dir.exists(path[1])
+  if (!ok) ok = dir.create(path, recursive = TRUE)
+  path
+}
